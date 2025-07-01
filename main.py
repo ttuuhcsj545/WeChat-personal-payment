e62a40fc0be9ce3b25671dd1169669fd0c583aaa
田志浩 <19391818981@163.com>
2025-07-01 18:12:52 +0800

    基本完善

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-from match_screen_template import match_screen_template
 from detect_qrcode_from_screen import detect_qrcode_from_screen
 from WeChat_status import get_wechat_window_info
 from detect_color_position import sift_match_on_screen
 from color import is_color_match_at_offset
-import os
 def start():
     if get_wechat_window_info():#检测微信登录状态
         zhi ,zuobiao= sift_match_on_screen("res/small.png")
@@ -18,9 +16,9 @@ def start():
     else:
         uis=detect_qrcode_from_screen()
         if uis ==False:
-            is_ma3,ssc3,hhh3=match_screen_template("res/Switch account.png")
-            if is_ma3 ==True:
-                return "004",ssc3#切换账号
+            zhi1 ,zuobiao1= sift_match_on_screen("res/Switch account.png")
+            if zhi1:
+                return "003",zuobiao1#切换账号
             else:
                 return "404_1"#未知
         else:
